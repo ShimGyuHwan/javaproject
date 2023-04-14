3b790b228c36d9fb2804b01cb2ac10411b66bebc
ShimGyuHwan <calcifer36@naver.com>
2023-04-14 10:37:25 +0900

    master

---
+++ java2/src/index2.java
@@ -0,0 +1,29 @@
+
+public class index2 {
+
+	//필드 공간
+	
+	public static void main(String[] args) {
+	
+		String a = "홍길동";
+		int b =10;
+		int c =20;
+		int d = b+c;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(d);
+		double e = 1.5;
+		System.out.println(e);
+		long f =12;
+		System.out.println(f);
+		// byte  : -128 ~127
+		// short : -32600 ~ 32600
+		// int : -2147000000 ~ 2147000000
+		// long : 전부 다
+		float num = 3.12394929394901020f;
+		System.out.println(num);
+		
+		
+	}
+	
+}
